b9f9233cc0922034caa2cc00b78715a2bfae59f0
amit <ajain@boisestate.edu>
2024-11-25 15:09:58 -0700

    fix search scripts

--- check-db-search.sh
+++ check-db-search.sh
@@ -3,13 +3,13 @@
 for value in accepted-ip accepted-time invalid-ip invalid-time failed-ip failed-time reverseaddress-ip reverseaddress-time user-ip
 do
 	echo
-	echo "Checking db search query results "DB-QUERY-"$value".txt""
-  	diff -w output/db-search/"DB-QUERY-"$value"-top25.txt" "results/db-search/"$value"-top25.txt""
+	echo "Checking db search query results DB-QUERY-$value.txt"
+    diff -w output/db-search/DB-QUERY-"$value"-top25.txt results/db-search/"$value"-top25.txt
   	if test "$?" = "0"
   	then
     	echo "----> Test-$value PASSED!"
 	else
-   		echo "----> Test-$value FAILED :( @#$%!"
+   		echo "----> Test-$value FAILED :("
   	fi
 done
 echo
