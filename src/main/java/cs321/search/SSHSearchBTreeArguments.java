b2e39a800746ed27bd816abdb07aa807205a7de4
DevynKore <devynkorber@gmail.com>
2025-04-12 13:53:56 -0600

    docs to start

--- src/main/java/cs321/search/SSHSearchBTreeArguments.java
+++ src/main/java/cs321/search/SSHSearchBTreeArguments.java
@@ -1,5 +1,46 @@
 package cs321.search;
 
+/**
+ * Parses and validates command-line arguments for the SSHSearchBTree program.
+ *
+ * This class is responsible for interpreting arguments passed to:
+ *
+ *   java -jar build/libs/SSHSearchBTree.jar
+ *
+ * Required command-line arguments:
+ *
+ *   --cache=<0|1>
+ *     Enables (1) or disables (0) the use of an in-memory cache for BTree nodes.
+ *
+ *   --degree=<btree-degree>
+ *     Specifies the degree of the BTree.
+ *     Use 0 to auto-calculate the optimal degree based on 4096-byte node size.
+ *
+ *   --btree-file=<btree-filename>
+ *     The path to the binary file containing the serialized BTree.
+ *
+ *   --query-file=<query-filename>
+ *     A file containing one query key per line to search within the BTree.
+ *
+ * Optional command-line arguments:
+ *
+ *   --top-frequency=<10|25|50>
+ *     If provided, only the top N results will be returned based on frequency.
+ *     Defaults to no frequency filtering if omitted.
+ *
+ *   --cache-size=<n>
+ *     The maximum number of BTreeNode objects that can be stored in memory.
+ *     Required if --cache=1. Must be between 100 and 10000.
+ *
+ *   --debug=<0|1>
+ *     Enables verbose debug mode (1) or runs normally (0).
+ *     Default is 0.
+ *
+ * Complete Format:
+ * java -jar build/libs/SSHSearchBTree.jar --cache=<0/1> --degree=<btree-degree> \
+          --btree-file=<btree-filename> --query-file=<query-fileaname> \
+          [--top-frequency=<10/25/50>] [--cache-size=<n>]  [--debug=<0|1>]
+ */
 
 public class SSHSearchBTreeArguments
 {
--- src/main/java/cs321/search/SSHSearchDatabaseArguments.java
+++ src/main/java/cs321/search/SSHSearchDatabaseArguments.java
@@ -1,4 +1,40 @@
 package cs321.search;
 
+/**
+ * Parses and validates command-line arguments for the SSHSearchDatabase program.
+ *
+ * This class is responsible for interpreting arguments passed to:
+ *
+ *   java -jar build/libs/SSHSearchDatabase.jar
+ *
+ * Required command-line arguments:
+ *
+ *   --type=<tree-type>
+ *     Specifies the category of log data to query.
+ *     Valid values:
+ *       accepted-ip
+ *       accepted-time
+ *       failed-ip
+ *       failed-time
+ *       invalid-ip
+ *       invalid-time
+ *       reverseaddress-ip
+ *       reverseaddress-time
+ *       user-ip
+ *       test         // Special mode to populate a test SQLite database
+ *
+ *   --database=<sqlite-database-path>
+ *     The path to the SQLite database file.
+ *     Example: SSHLogDB.db
+ *
+ *   --top-frequency=<10|25|50>
+ *     The number of most frequent keys to return from the database.
+ *
+ * Complete Format:
+ * java -jar build/libs/SSHSearchDatabase.jar --type=<tree-type> \
+          --database=<sqlite-database-path> --top-frequency=<10/25/50>
+ */
+
+
 public class SSHSearchDatabaseArguments {
 }
